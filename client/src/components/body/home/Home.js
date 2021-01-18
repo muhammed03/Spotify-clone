22f32af57041deecc46d444139837b368f82375c
Muhammedamin <muhammed03amin@gmail.com>
2021-01-18 23:42:00 +0600

    feat: add authentication page, set-up backend, handle users data

---
+++ client/src/components/body/home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+    return (
+        <div className="home_page">
+            <h2>Hello everyone!</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In metus vulputate eu scelerisque felis imperdiet. Justo eget magna fermentum iaculis eu. Hendrerit dolor magna eget est lorem ipsum. Adipiscing elit pellentesque habitant morbi. Laoreet id donec ultrices tincidunt arcu non sodales neque. Aliquam sem fringilla ut morbi tincidunt augue. Euismod nisi porta lorem mollis aliquam. Amet est placerat in egestas. Neque vitae tempus quam pellentesque nec nam aliquam. Imperdiet sed euismod nisi porta. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam. Suspendisse interdum consectetur libero id faucibus nisl tincidunt. Nulla aliquet enim tortor at auctor urna. Mi eget mauris pharetra et ultrices neque ornare. Turpis cursus in hac habitasse platea.
+            </p>
+            <a href="https://github.com/muhammed03" target="_blank"
+                rel="noopener noreferrer">My Github</a>
+        </div>
+    )
+}
+
+export default Home
